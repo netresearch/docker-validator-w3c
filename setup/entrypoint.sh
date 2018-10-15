832dc92a7b526e3c3f7b6054719673d741ca6b53
Sebastian Mendel <sebastian.mendel@netresearch.de>
2018-10-15 11:53:14 +0200

    NRTECH-3442: clean /var/run/apache2 on every start - fixes HTTP 503: 'Address already in use: AH01243: Couldn't bind unix domain socket /var/run/apache2/cgisock.9'

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -3,6 +3,7 @@ set -ex
 
 # no arguments at all - start apache as entry point command
 if [ $# -eq 0 ]; then
+    rm -rf /var/run/apache2
     set -- apachectl -D "FOREGROUND" -k start >> /dev/stdout 2>&1 "$@"
 fi
 
